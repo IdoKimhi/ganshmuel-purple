95c46cd92dd3a2c21669b1937150625a65dd1861
IdoKimhi <ido23131@gmail.com>
2025-11-22 12:51:10 +0200

    add webhook listener

--- devops/app.py
+++ devops/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import json
+import os
 
 app = Flask(__name__)
 
@@ -7,6 +8,14 @@ app = Flask(__name__)
 def health_check():
     return "OK", 200
 
+
+@app.route("/webhookcallback", methods=["POST"])
+def hook():
+    print("Received GitHub Webhook Headers:", request.headers)
+    print("Received GitHub Webhook JSON:", request.get_json(silent=True))
+    return "Webhook Received by Flask Dev Server", 200
+
+
 if __name__ == '__main__':
     # Flask app will run on port 8000 (standard for a simple server process)
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8080)
