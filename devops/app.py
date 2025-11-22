0b09a373b500e6711afac0607f6a71b045674448
ido-kimchi <ido23131@gmail.com>
2025-11-22 19:13:52 +0200

    add webhook handler

--- devops/app.py
+++ devops/app.py
@@ -15,6 +15,35 @@ def hook():
     print("Received GitHub Webhook JSON:", request.get_json(silent=True))
     return "Webhook Received by Flask Dev Server", 200
 
+@app.route('/trigger', methods=['POST'])
+
+#check for json content type
+if not request.is_json:
+    return jsonify ({"message": "Content-Type must be application/json"}), 400
+
+data = request.get_json()
+
+action = data.get('action')
+pusher_data = data.get('pusher')
+
+repository_data = data.get('reoisitory', {})
+branches_url = repository_data.get('branches_url')
+
+#process the extracted data
+
+if action and pusher_data and branches_url:
+    pusher_username = pusher_data.get('name', 'N/A')
+    print("--- GitHub Webhook Received ---")
+    print(f"Action: **{action}**")
+    print(f"Pusher: **{pusher_username}**")
+    print(f"Branches URL: **{branches_url}**")
+    print("-------------------------------")
+
+    #logic goes here, if action == 'created':...
+    return jsonify({"message": "Webhook successfully processed"}), 200
+else:
+    print(f"Error: Missing data in payload. Action: {action}, Pusher: {pusher_data is not None}, Branches URL: {branches_url}")
+    return jsonify({"message": "Missing required data in payload"}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
