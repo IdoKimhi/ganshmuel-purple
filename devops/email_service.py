edf81a32058bc19c538553e3069b5737758d2851
IdoKimhi <ido23131@gmail.com>
2025-11-23 09:30:34 +0200

    update email_service.py, update .gitignore

--- devops/email_service.py
+++ devops/email_service.py
@@ -2,16 +2,23 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def send_notification_email(pusher_name, commit_url):
-    # --- CONFIGURATION ---
-    # ideally, load these from os.environ for security
-    SMTP_SERVER = "smtp.gmail.com" # Example for Gmail
-    SMTP_PORT = 587
-    SENDER_EMAIL = "your_email@gmail.com"
-    SENDER_PASSWORD = "your_app_password" # Do NOT use your login password, use an App Password
-    RECEIVER_EMAIL = "your_email@gmail.com"
 
+    SMTP_SERVER = "smtp.gmail.com" 
+    SMTP_PORT = 587
+    SENDER_EMAIL = os.getenv('EMAIL_USER')
+    SENDER_PASSWORD = os.getenv('EMAIL_PASS')
+    RECEIVER_EMAIL = os.getenv('EMAIL_USER') #sending to myself
+
+# Check if credentials exist before proceeding
+    if not SENDER_EMAIL or not SENDER_PASSWORD:
+        print("Error: Email credentials not found in environment variables.")
+        return
+    
     subject = f"Alert: New Push to 'devops' branch by {pusher_name}"
     
     body = f"""
@@ -29,16 +36,17 @@ def send_notification_email(pusher_name, commit_url):
     msg.attach(MIMEText(body, 'html'))
 
     try:
-        # Connect to the server
         server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-        server.starttls() # Secure the connection
+        server.starttls()
         server.login(SENDER_EMAIL, SENDER_PASSWORD)
-        
-        # Send email
         server.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, msg.as_string())
         print(f"Email successfully sent to {RECEIVER_EMAIL}")
-        
     except Exception as e:
         print(f"Failed to send email: {e}")
     finally:
-        server.quit()
+        # Standard practice: Check if server exists before quitting 
+        # to avoid errors if connection failed early
+        try:
+            server.quit()
+        except:
+            pass
