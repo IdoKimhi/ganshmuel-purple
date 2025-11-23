4d6187b35be7fc198475f4fb10a596f1b136ca17
IdoKimhi <ido23131@gmail.com>
2025-11-23 23:16:22 +0200

    test

--- ci.sh
+++ ci.sh
@@ -9,7 +9,7 @@ echo "1. Getting latest changes from Git..."
 # Use git pull --rebase to fetch new changes and update local files.
 # This is CRITICAL for self-update, as it pulls the new code (including the new app.py)
 # before the Docker image is rebuilt.
-git pull --rebase && chmod +x ci.sh || { echo "ERROR: Git pull failed. Stopping startup process."; exit 1; }
+git pull --rebase || { echo "ERROR: Git pull failed. Stopping startup process."; exit 1; }
 
 # Assuming the compose files are in a 'devops' subdirectory relative to where ci.sh is executed.
 CI_COMPOSE="devops/docker-compose-ci-server.yml"
@@ -25,7 +25,7 @@ echo "3. Building and starting Production Environment in detached mode (-d)..."
 # This ensures the Production environment is running constantly.
 # We assume docker-compose-prod.yml exists and defines the app stack.
 docker compose -f $PROD_COMPOSE up -d --build --force-recreate
-##
+#
 echo "------------------------------------------------------"
 echo "CI Service (Port 8080) and Production Environment are now running."
 echo "------------------------------------------------------"
